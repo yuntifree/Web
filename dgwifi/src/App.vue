781eb03329470b291916aff14c5562f017c4918a
chunyanzeng <2218583181@qq.com>
2016-11-09 18:10:07 +0800

    dgwifi

---
+++ dgwifi/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/newslist'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './assets/css/reset.css';
+</style>
---
+++ dgwifi/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+var initFont = require('./common/font.js')
+initFont(true);
+
+var infiniteScroll =  require('vue-infinite-scroll');
+Vue.use(infiniteScroll)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
