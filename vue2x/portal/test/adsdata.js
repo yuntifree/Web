8832cd0f81da0c5f321b664bdbd446c97d19de9a
chunyanzeng <2218583181@qq.com>
2017-01-05 17:50:56 +0800

    act_banner url

--- vue2x/portal/test/adsdata.js
+++ vue2x/portal/test/adsdata.js
@@ -1,6 +1,6 @@
 // login user
 var ads = [{
-    img: 'http://img.yunxingzh.com/5f058f74-3284-4ecd-bfc9-c995c95604f1.png',
+    img: '../static/images/act_banner.png',
     url: 'http://a.app.qq.com/o/simple.jsp?pkgname=com.yunxingzh.wireless'
   }]
 
