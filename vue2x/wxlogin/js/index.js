e0f44ac61a16d66f499952dfbf811e95163719da
hzgnpu <tinylee@qq.com>
2017-03-30 18:33:25 +0800

    wx login

--- vue2x/wxlogin/js/index.js
+++ vue2x/wxlogin/js/index.js
@@ -8,12 +8,12 @@ var ads = {
 var timer;
 
 var query = CGI.query();
-var wlanacname = query.wlanacname || '';//'2043.0769.200.00';
-var wlanuserip = query.wlanuserip || '';//'10.96.72.28';
-var wlanacip = query.wlanacip || '';//'120.197.159.10';
-var wlanusermac = query.wlanusermac || '';//'f45c89987347';
+var wlanacname = query.wlanacname || ''; //'2043.0769.200.00';
+var wlanuserip = query.wlanuserip || ''; //'10.96.72.28';
+var wlanacip = query.wlanacip || ''; //'120.197.159.10';
+var wlanusermac = query.wlanusermac || ''; //'f45c89987347';
 var wlanapmac = query.wlanapmac || '';
-var firsturl = query.wlanuserfirsturl || 'http://www.baidu.com';//'http://www.baidu.com';
+var firsturl = query.wlanuserfirsturl || 'http://www.baidu.com'; //'http://www.baidu.com';
 var autologin = 0;
 //var useragent = navigator.userAgent;
 var canClick = true;
@@ -21,15 +21,24 @@ var canClick = true;
 //
 var appId = "wxbf43854270af39aa";
 var secretkey = "f1d41ba80597ee59b142032e16f801d9";
-var extend = [wlanacname,wlanuserip,wlanacip,wlanusermac,wlanapmac].join(',');    　　　 //开发者自定义参数集合
-var timestamp = new Date().getTime();　　　　//时间戳(毫秒)
-var shop_id = "4040455";            　　  //AP设备所在门店的ID
-var authUrl = "http://wx.yunxingzh.com/auth";        //认证服务端URL
-var mac = query.wlanusermac || '';  　　　//用户手机mac地址 安卓设备必需
-var ssid = "";           //AP设备信号名称，非必须
+var extend = [wlanacname, wlanuserip, wlanacip, wlanusermac, wlanapmac].join(',');　　　 //开发者自定义参数集合
+var timestamp = new Date().getTime();　　　　 //时间戳(毫秒)
+var shop_id = "4040455";　　 //AP设备所在门店的ID
+var authUrl = "http://wx.yunxingzh.com/auth"; //认证服务端URL
+var mac = query.wlanusermac || '';　　　 //用户手机mac地址 安卓设备必需
+var ssid = ""; //AP设备信号名称，非必须
 var bssid = "";
 var jumpUrl = "";
+var connected = false;
 
+
+var loginParam = {
+  wlanacname: wlanacname,
+  wlanuserip: wlanuserip,
+  wlanacip: wlanacip,
+  wlanusermac: wlanusermac,
+  wlanapmac: wlanapmac
+};
 
 function errorJump() {
   if (jumpUrl.length > 0) {
@@ -90,78 +99,6 @@ var JPlaceHolder = {
   }
 };
 
-(function() {
-  font(true);
-  String.prototype.trim = function() {
-    return this.replace(/(^\s*)|(\s*$)/g, "");
-  }
-  var param = {
-    wlanusermac: wlanusermac,
-    wlanacname: wlanacname,
-    wlanapmac: wlanapmac,
-  };
-  var screenWidth = document.documentElement.clientWidth;
-  CGI.get('check_login', param, function(resp) {
-    if (resp.errno == 0) {
-      var data = resp.data;
-      autologin = data.autologin;
-      if (isPC()) {
-        if (autologin) {
-          $('.login').append(template('tplPhone', {}));
-        } else {
-          $('.login').append(template('tplPc', {}));
-        }
-        var height = document.documentElement.clientHeight;
-        $('html').css('height',height);
-        $('.login').css('height','100%');
-       } else {
-        if (autologin) {
-          $('.login').append(template('tplOnelogin', data));
-          $('.login').append(template('tplBottom', ads));
-        } else {
-          $('.login').append(template('tplIptlogin', data));
-          $('.login').append(template('tplBottom', ads));
-        }
-        setTimeout(function(){setHeight()},300);
-      }
-      initUI();
-    } else {
-      tipShow(resp.desc);
-    }
-  });
-})()
-
-function setHeight() {
-  var height = document.documentElement.clientHeight;
-  var htmlHeight = $('html').height();
-  if (height > htmlHeight) {
-   $('html').css('height',height);
-    $('.login-bottom').css('position','absolute');
-    $('.login-bottom').css('left', 0);
-    $('.login-bottom').css('bottom', 0);
-  }
-}
-
-
-function initUI() {
-  $('.ipt-phone').focus();
-  JPlaceHolder.init();
-  $('.query-code').click(getCode);
-  if (isPC()) {
-    $('.pc-btn').click(touchEnd);
-  } else {
-    if (autologin) {
-      touchEnd();
-      $('.wx-btn').get(0).addEventListener('touchstart', touchStart, false);
-      $('.wx-btn').get(0).addEventListener('touchend', enableButton); 
-    } else {
-      $('.mob-btn').get(0).addEventListener('touchstart', touchStart, false);
-      $('.mob-btn').get(0).addEventListener('touchend', touchEnd, false);
-    }
-  }
-  $('.has-ques').click(getQues)
-}
-
 function font(bFont) {
   if (document && window) {
     // init font
@@ -186,6 +123,82 @@ function font(bFont) {
       window.addEventListener(resizeEvt, recalc, false);
     }
   }
+}
+
+// -----------------------------------
+(function() {
+  font(true);
+  String.prototype.trim = function() {
+    return this.replace(/(^\s*)|(\s*$)/g, "");
+  }
+  var param = {
+    wlanusermac: wlanusermac,
+    wlanacname: wlanacname,
+    wlanapmac: wlanapmac,
+  };
+  var screenWidth = document.documentElement.clientWidth;
+  CGI.get('check_login', param, function(resp) {
+    if (resp.errno == 0) {
+      var data = resp.data;
+      autologin = data.autologin;
+      if (isPC()) {
+        if (autologin) {
+          $('.login').append(template('tplPhone', {}));
+        } else {
+          $('.login').append(template('tplPc', {}));
+        }
+        var height = document.documentElement.clientHeight;
+        $('html').css('height', height);
+        $('.login').css('height', '100%');
+      } else {
+        if (autologin) {
+          $('.login').append(template('tplOnelogin', data));
+          $('.login').append(template('tplBottom', ads));
+        } else {
+          $('.login').append(template('tplIptlogin', data));
+          $('.login').append(template('tplBottom', ads));
+        }
+        setTimeout(function() {
+          setHeight()
+        }, 300);
+      }
+      initUI();
+    } else {
+      tipShow(resp.desc);
+    }
+  });
+})()
+
+function setHeight() {
+  var height = document.documentElement.clientHeight;
+  var htmlHeight = $('html').height();
+  if (height > htmlHeight) {
+    $('html').css('height', height);
+    $('.login-bottom').css('position', 'absolute');
+    $('.login-bottom').css('left', 0);
+    $('.login-bottom').css('bottom', 0);
+  }
+}
+
+function initUI() {
+  $('.ipt-phone').focus();
+  JPlaceHolder.init();
+  $('.query-code').click(getCode);
+  if (isPC()) {
+    $('.pc-one-btn').click(pcOneClick);
+    $('.pc-reg-btn').click(pcRegClick);
+  } else {
+    if (autologin) {
+      $('.wx-btn').get(0).addEventListener('touchstart', touchStart, false);
+      $('.wx-btn').get(0).addEventListener('touchend', mobOneClick, false);
+      // 发送自动认证请求, 不调用微信
+      oneClickLogin(true, false);
+    } else {
+      $('.mob-btn').get(0).addEventListener('touchstart', touchStart, false);
+      $('.mob-btn').get(0).addEventListener('touchend', mobRegClick, false);
+    }
+  }
+  $('.has-ques').click(getQues)
 }
 
 function isPC() {
@@ -255,56 +268,69 @@ function getCode() {
 
 function touchStart(e) {
   $('.btn').css('backgroundColor', '#0187ee');
+  setTimeout(function() {$('.btn').css('backgroundColor', '#00a0fb');}, 300);
 }
 
-function touchEnd(e) {
+function pcRegClick(e) {
   if (canClick) {
-    var param = {
-      wlanacname: wlanacname,
-      wlanuserip: wlanuserip,
-      wlanacip: wlanacip,
-      wlanusermac: wlanusermac,
-      wlanapmac: wlanapmac
-    };
-
-    if (autologin) {
-      disableButton();
-      oneClickLogin(param, !isPC());
-    } else {
-      var phone = $('.ipt-phone').val().trim();
-      var code = $('.ipt-code').val().trim();
-      if (checkPhone(phone)) {
-        param.phone = phone;
-        if (code.length <= 0) {
-          tipShow('请输入验证码');
-          enableButton();
-        } else {
-          param.code = code;
-          disableButton();
-          portalLogin(param, !isPC());
-        }
-      }
-    }
+    disableButton();
+    regClick(false);
   }
 }
 
-function enableButton() {
-  $('.btn').css('backgroundColor', '#00a0fb');
+function mobRegClick(e) {
+  if (canClick) {
+    disableButton();
+    regClick(true);
+  }
 }
 
-function disableButton() {
-  canClick = false;
-  setTimeout(function() {
-    canClick = true;
-  },3000)
+function pcOneClick(e) {
+  if (canClick) {
+    disableButton();
+    oneClickLogin(false, false);
+  }
 }
 
+function mobOneClick(e) {
+  if (canClick) {
+    disableButton();
+    oneClickLogin(false, true);
+  }
+}
 
-function portalLogin(param,wx) {
+function regClick(wx) {
+  var param = loginParam;
+  var phone = $('.ipt-phone').val().trim();
+  var code = $('.ipt-code').val().trim();
+  if (checkPhone(phone)) {
+    param.phone = phone;
+    if (code.length <= 0) {
+      tipShow('请输入验证码');
+    } else {
+      param.code = code;
+      portalLogin(param, wx);
+    }
+  } else {
+    // tip in checkphone
+  }
+}
+
+function portalLogin(param, wx) {
   CGI.get('portal_login', param, function(resp) {
-    enableButton();
     if (resp.errno === 0) {
-      loginDone(resp.data, wx);
+      genJumpUrl(resp.data);
+      connected = true;
+      sendTest();
+      if (wx) {
+        setTimeout(function() {callWeixin()}, 300);
+      } else {
+        if (CGI.isIE()) {
+          location.replace(firsturl);
+        } else {
+          location.replace(jumpUrl);
+        }
+      }
     } else {
       // 验证码
       tipShow(resp.desc);
@@ -316,41 +342,74 @@ function portalLogin(param,wx) {
   })
 }
 
-function oneClickLogin(param, wx) {
+/**
+ * [oneClickLogin description]
+ * @param  {[type]} auto   是否自动点击
+ * @param  {[type]} callWX 是否调用微信
+ */
+function oneClickLogin(auto, callWX) {
+  var param = loginParam;
   CGI.get('one_click_login', param, function(resp) {
-    enableButton();
     if (resp.errno === 0) {
-      loginDone(resp.data, wx);
+      // 局部刷新，通知iOS能上网了
+      genJumpUrl(resp.data);
+      sendTest();
+      connected = true;
+      if (!auto) {
+        // 如果需要，调用微信
+        if (callWX) {
+          setTimeout(function() {callWeixin()}, 300);
+        } else {
+          if (CGI.isIE()) {
+            location.replace(firsturl);
+          } else {
+            location.replace(jumpUrl);
+          }
+        }
+      } else {
+        // do nothing
+      }
     } else {
-      // 验证码
       tipShow(resp.desc);
     }
   })
 }
 
-function loginDone(info, wx) {
+function callWeixin() {
+  if (connected) {
+    callWechatBrowser();
+    setTimeout(function() {
+      errorJump();
+    }, 3000)
+  } else {
+  }
+}
+
+function disableButton() {
+  canClick = false;
+  setTimeout(function() {
+    canClick = true;
+  }, 3000)
+}
+
+// make apple happy!
+function sendTest() {
+  CGI.get('test', {}, function(resp) {
+    // empty
+  })
+}
+
+function genJumpUrl(info) {
   $('.ipt-phone').val('');
   $('.ipt-code').val('');
   var dir = info.portaldir;
-  var paramCode = "?uid=" + info.uid + '&token=' + info.token + '&adtype='+ info.adtype+ '&portaltype='+ info.portaltype+'&ts=' + ~~((new Date()).getTime()/1000) + '&s=1'
+  var paramCode = "?uid=" + info.uid + '&token=' + info.token + '&adtype=' + info.adtype + '&portaltype=' + info.portaltype + '&ts=' + ~~((new Date()).getTime() / 1000) + '&s=1'
   if (!info.portaltype) {
     if (dir.indexOf('.html') === -1) {
       dir = info.portaldir + '/wifilink.html'
     }
   }
   jumpUrl = dir + paramCode;
-  if (CGI.isIE()) {
-    location.replace(firsturl);
-  } else {
-    if (wx) {
-      callWechatBrowser();
-      setTimeout(function() {
-        errorJump();
-      }, 3000)
-    } else {
-      location.replace(jumpUrl);
-    }
-  }
 }
 
 function callWechatBrowser() {
@@ -359,7 +418,7 @@ function callWechatBrowser() {
 }
 
 function getQues() {
-  var url = 'http://120.76.236.185/wx/h5/question.html?wlanacname='+ wlanacname +
-      '&wlanusermac='+wlanusermac+'&wlanapmac='+wlanapmac + '&ts=' + (new Date()).getTime();
+  var url = 'http://120.76.236.185/wx/h5/question.html?wlanacname=' + wlanacname +
+    '&wlanusermac=' + wlanusermac + '&wlanapmac=' + wlanapmac + '&ts=' + (new Date()).getTime();
   location.href = url;
 }
